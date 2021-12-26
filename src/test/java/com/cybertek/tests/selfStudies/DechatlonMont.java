20c38994d3b9daef7382a34618fb71644a377e93
Cihan ASLAN <aslan2341@gmail.com>
2021-12-26 14:46:43 +0300

    Dechatlon Mont alımı adımları v1

--- src/test/java/com/cybertek/tests/selfStudies/DechatlonMont.java
+++ src/test/java/com/cybertek/tests/selfStudies/DechatlonMont.java
@@ -3,7 +3,7 @@ package com.cybertek.tests.selfStudies;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
