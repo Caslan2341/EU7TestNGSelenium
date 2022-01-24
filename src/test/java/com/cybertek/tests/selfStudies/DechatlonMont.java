e7c68c4e6423c08808847fcfb85a65b655898e89
Cihan ASLAN <aslan2341@gmail.com>
2022-01-25 00:04:47 +0300

    self study update

--- src/test/java/com/cybertek/tests/selfStudies/DechatlonMont.java
+++ src/test/java/com/cybertek/tests/selfStudies/DechatlonMont.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
